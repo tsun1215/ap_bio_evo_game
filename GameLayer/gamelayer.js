000c8b76c26925f4e7d7fe57ff82279630725bbd
Tyuguy <shuyun.ren@gmail.com>
2013-05-25 18:15:42 -0700

    Random stuffs

--- GameLayer/gamelayer.js
+++ GameLayer/gamelayer.js
@@ -15,6 +15,7 @@ function initScreen() {
 	stage = new createjs.Stage("screen");
 	createjs.Ticker.addEventListener("tick", update);
 	createjs.Ticker.setInterval(interval);
+	this.addEventListener("mousedown", mouseHandler);
 }
 
 function update() {
@@ -22,6 +23,11 @@ function update() {
 		sList[i].migrateOnce(interval);
 		sList[i].draw();
 	}
+}
+
+function mouseHandler(event){
+	
+
 }
 
 var Settlement = function(pop, xCoord, yCoord) {
@@ -37,6 +43,28 @@ var Settlement = function(pop, xCoord, yCoord) {
 var Circle = function() {
 
 }
+
+var migrateOnce = function(fpsDelta){
+	if (destination != null){
+		// flipped accounts for problems with arctan's range only being -pi/2 to pi/2
+		// either 1 (atan works) or -1 (atan doesn't work)
+		var flipped = 1;
+		var destX = this.destination.x;
+		var destY = this.destination.y;
+		var angle = Math.atan((destY-this.y)/(destX-this.x));
+		if (destX - this.x < 0){
+			flipped = -1;
+		}
+		var totalMovement = fpsDelta * this.speed;
+		this.x += flipped*totalMovement*cos(angle);
+		this.y += flipped*totalMovement*sin(angle);
+	}
+}
+
+var draw = function(){
+	this.shape.graphics.beginFill("blue").drawCircle(this.x, this.y, this.population/50);
+}
+
 
 var Loc = function(xCoord, yCoord){
 	var x;
