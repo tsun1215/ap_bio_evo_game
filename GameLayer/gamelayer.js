0ade575de13dab3fc44da6be25fbac77151073d1
Tyuguy <shuyun.ren@gmail.com>
2013-05-26 08:08:25 -0700

    Added population dynamics

--- GameLayer/gamelayer.js
+++ GameLayer/gamelayer.js
@@ -6,6 +6,7 @@ var focus = null;
 var sight;
 var popAdjuster;
 var popText;
+var mapArr;
 
 Settlement.prototype = new createjs.Shape();
 Settlement.prototype.constructor = Settlement;
@@ -17,16 +18,14 @@ function initGame() {
 	console.log("chkpt 2");
 	sList = new Array();
 	initmap();
-	new Settlement(100,5,5);
-	new Settlement(200,10,20);
-	new Settlement(200,100,200);
+	new Settlement(200,100,200, mapArr);
 	initScreen();
 	initSight();
 	initPopAdjuster();
 }
 
 function initmap(){
-	var mapArr = new Map(200,150,10, [Math.random()*10000,Math.random()*10000,Math.random()*10000]);
+	mapArr = new Map(50,50,10, [Math.random()*10000,Math.random()*10000,Math.random()*10000]);
 	mapArr.generate();
 	map = new createjs.Container();
 	map.x = -(mapArr.cols*mapArr.tile_width)/2;
@@ -110,22 +109,32 @@ function initScreen() {
 }
 
 function refresh(event) {
+	if(createjs.Ticker.getTicks() % 90 == 0){
+		for(i in sList){
+			sList[i].survival();
+		}
+	}
 	for(i in sList) {
 		sList[i].migrateOnce(event.delta/10);
-	}
+		}
 	stage.update(event);
 }
 
+// function survive(event) {
+// 	for(i in sList) {
+// 		sList[i].survival();
+// 	}
+// }
 function mouseHandler(event){
-	console.log("SOME EVENT HAPPENED");
+	//console.log("SOME EVENT HAPPENED");
 	if (event.type == "click"){
-		console.log(sight.x);
+		//console.log(sight.x);
 		if (focus != null){
-			console.log("Secondclicked");
-			console.log(focus.movingPop);
-			console.log(sight.alpha);
+			//console.log("Secondclicked");
+			//console.log(focus.movingPop);
+			//console.log(sight.alpha);
 			if (focus.movingPop > 0){
-				console.log("focus.movingPop > 0");
+				//console.log("focus.movingPop > 0");
 				var loc = stage.globalToLocal(event.stageX, event.stageY);
 				if (focus.movingPop < focus.population){	
 					var settle = new Settlement(focus.movingPop, focus.x, focus.y, focus.map);
@@ -149,49 +158,106 @@ function mouseHandler(event){
 
 		} else {
 			focus = event.target;
-			console.log(focus);
+			//console.log(focus);
 			aimSight(event.stageX, event.stageY);
 			sight.alpha = .5;
 			popAdjuster.alpha = 1;
-			console.log(sight.alpha);
-			console.log("first clicked");
-			console.log(focus);
+			//console.log(sight.alpha);
+			//console.log("first clicked");
+			//console.log(focus);
 			stage.addEventListener("stagemousemove", stageEventHandler);
 		}
 	}
 }
 
 function stageEventHandler(event){
-	console.log("stageevent called");
+	//console.log("stageevent called");
 	if (event.type == "stagemousemove"){
+		//console.log("stagemousemoved");
 		var loc = stage.globalToLocal(event.stageX, event.stageY);
 		aimSight(loc.x, loc.y);
 		aimPopAdjuster();
 		updatePopAdjuster();
-
 	}	
 }
+// 0 = % heat pref
+// 1 = % water needy
+// 2 = % nutrient dependent
+var TraitsList = function(heat, water, nutrient){
+	this.list = new Array();
+	this.list[0] = heat;
+	this.list[1] = water;
+	this.list[2] = nutrient;
+}
 
-function Settlement(pop, xCoord, yCoord, map,idealT,tempResist) {
+function Settlement(pop, xCoord, yCoord, amap) {
 	this.population = pop;
 	this.movingPop = pop;
 	this.x = xCoord;
 	this.y = yCoord;
+	this.traits = new TraitsList(.15, .35, .7);
 	this.destinationX;
 	this.destinationY;
 	this.speed;
 	this.movingPop;
 	this.migrateOnce = migrateOnce;
 	this.addEventListener("click", mouseHandler);
-	this.graphics.beginStroke("black").beginFill("red").drawCircle(0,0,8);
+	var color = rgbToHex(Math.floor(255*this.traits.list[0]),Math.floor(255*this.traits.list[1]),Math.floor(255*this.traits.list[2]))
+	this.graphics.beginStroke("black").beginFill(color).drawCircle(0,0,8);
 	stage.addChild(this);
 	sList.push(this);
+	this.map = amap;
 }
 
-Settlement.prototype.survival = function(map){
-	var netGrowth = (this.tempResist - Math.abs(Math.floor((this.map.tiles[this.xCoord / 16][this.yCoord / 16].temperature) * 100))) / this.tempResist;
-	var newPop = netGrowth * interval + this.population;
-	this.population = newPop;
+Settlement.prototype.survival = function(){
+	
+	var calcInt = interval;
+	var temp =  this.map.tiles[Math.floor(this.x / 16)][Math.floor(this.y / 16)].attributes.temperature * 100;
+	console.log(temp);
+	var domGrowthRate;
+	var recGrowthRate;
+	var calcDiff = ((Math.sqrt(Math.abs(50-temp))) + calcInt - 5)/ calcInt;
+	//console.log(calcDiff);
+	if (temp > 50){
+		domGrowthRate = this.traits.list[0] * calcDiff;
+		recGrowthRate = (1-this.traits.list[0]) * (1.9-calcDiff); 
+	}
+	if (temp < 50){
+		domGrowthRate = this.traits.list[0] * (1.9-calcDiff);
+		recGrowthRate = (1-this.traits.list[0]) * calcDiff; 
+	}
+	console.log(domGrowthRate);
+	console.log(recGrowthRate);
+
+	// var domGrowthRate = this.traits.list[0] * (Math.sqrt(temp - 0) + calcInt - 5)/calcInt;
+	// console.log(domGrowthRate);
+	// var recGrowthRate = (1-this.traits.list[0]) * (Math.sqrt(100-temp) + calcInt - 5)/calcInt;
+	// console.log(recGrowthRate);
+	// var domDeathRate = this.traits.list[0] * (Math.sqrt(100 - temp) + calcInt - 5)/calcInt;
+	// console.log(domDeathRate);
+	// var recDeathRate = (1-this.traits.list[0]) * (Math.sqrt(temp-0) + calcInt - 5)/calcInt;
+	// console.log(recDeathRate);
+	var totalGrowth = domGrowthRate + recGrowthRate;
+	console.log("totalGrowth = " + totalGrowth);
+
+	// var totalDeath = domDeathRate + recDeathRate;
+	// console.log("totalDeath = " + totalDeath);
+
+	// var totalChangeRate = totalGrowth - totalDeath;
+	// console.log("totalChange = " + totalChangeRate);
+
+	this.population = Math.floor(this.population*totalGrowth);
+	this.movingPop = this.population;
+	
+
+	// this.traits[0] = (domGrowthRate - domDeathRate)/(recGrowthRate - recDeathRate);
+
+	// if (Math.floor(this.map.tiles[this.xCoord / 16][this.yCoord / 16].temperature) > 50){
+
+	// }
+	// var netGrowth = (this.tempResist - Math.abs(Math.floor((this.map.tiles[this.xCoord / 16][this.yCoord / 16].temperature) * 100))) / this.tempResist;
+	// var newPop = netGrowth * interval + this.population;
+	// this.population = newPop;
 }
 
 var migrateOnce = function(speed){
