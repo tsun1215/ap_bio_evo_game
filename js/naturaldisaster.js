2021aa831f80bf1eb094efc75e26e97a54fba4a5
Terence Sun <tsun1215@gmail.com>
2013-06-06 22:24:12 -0700

    fixed mutilation

--- js/naturaldisaster.js
+++ js/naturaldisaster.js
@@ -35,6 +35,6 @@ Map.prototype.disasterRun = function(){
 	} else {
 		disastSeverity = 1.5;
 	}
-	// this.mutilate(disastSeverity, factorAffected, 500);
+	this.mutilate(disastSeverity, factorAffected, 1500);
 	currentDisaster = null;
 }
